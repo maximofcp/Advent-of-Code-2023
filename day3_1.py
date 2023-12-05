f5c47598813c2d9fac8da72db15f8716f83a1fa5
Filipe Paulo <maximofcp@gmail.com>
2023-12-05 11:44:29 +0000

    day3

---
+++ day3_1.py
@@ -0,0 +1,40 @@
+from itertools import chain
+import re
+from common import read_input
+
+
+numbers_pattern = re.compile(r'\d+')
+special_chars_pattern = re.compile(r'[^A-Za-z0-9.]')
+
+
+def find_number_intervals_indexes(engine_line: str) -> list[tuple[int, int]]:
+    return [(m.start(0), m.end(0)) for m in re.finditer(numbers_pattern, engine_line)]
+
+
+def find_special_characters_indexes(engine_line: str) -> list[int]:
+    return [m.start(0) for m in re.finditer(special_chars_pattern, engine_line)]
+
+
+def number_belongs_to_engine(number_indexes: tuple[int, int], engine_lines: list[str]) -> bool:
+    all_line_indexes = list(chain.from_iterable([find_special_characters_indexes(line) for line in engine_lines]))
+
+    return any(number_indexes[0] - 1 <= i <= number_indexes[1] for i in all_line_indexes)
+
+
+if __name__ == '__main__':
+    engine_schematics: list[str] = read_input('day3_1')
+    total_sum_of_engine_parts: int = 0
+    saved_numbers: list[str] = []
+
+    for line_index, line in enumerate(engine_schematics):
+        for start, end in find_number_intervals_indexes(line):
+            number_key = f'{line_index}-{start}-{end}'
+            if number_key in saved_numbers:
+                continue
+
+            saved_numbers.append(number_key)
+            surrounding_engine_lines = engine_schematics[max(line_index-1, 0):min(line_index+2, len(engine_schematics))]
+            if number_belongs_to_engine((start, end), surrounding_engine_lines):
+                total_sum_of_engine_parts += int(line[start:end])
+
+    print(total_sum_of_engine_parts)
---
+++ day3_2.py
@@ -0,0 +1,60 @@
+from itertools import chain
+import re
+from common import read_input
+from day3_1 import find_special_characters_indexes, numbers_pattern
+
+star_pattern = re.compile(r'[*]')
+
+
+def find_star_indexes(engine_line: str) -> list[int]:
+    return [m.start(0) for m in re.finditer(star_pattern, engine_line)]
+
+
+def find_number_intervals_indexes(engine_line: str, line_index: int) -> list[tuple[int, int, int]]:
+    return [(m.start(0), m.end(0), line_index) for m in re.finditer(numbers_pattern, engine_line)]
+
+
+def create_star_key(star_index: int, engine_index: int) -> str:
+    return f'{engine_index}-{star_index}'
+
+
+def find_star_gear_ratios(star_indexes: list[int], engine_lines: list[str]) -> int:
+    all_numbers_for_engine_lines = list(chain.from_iterable(
+        [find_number_intervals_indexes(line, i) for i, line in enumerate(engine_lines)]))
+
+    ratio_powers: list[int] = []
+
+    for star_index in star_indexes:
+        ratio = 1
+        ratio_count = 0
+        for number_start, number_end, line_index in all_numbers_for_engine_lines:
+            intersects = number_start >= star_index - 1 and number_start <= star_index + 1 or number_end > star_index -1 and number_end <= star_index + 1
+            if intersects:
+                ratio *= int(engine_lines[line_index][number_start:number_end])
+                ratio_count += 1
+
+        if ratio_count == 2:
+            ratio_powers.append(ratio)
+
+    return sum(ratio_powers)
+
+
+def number_belongs_to_engine(number_indexes: tuple[int, int], engine_lines: list[str]) -> bool:
+    all_line_indexes = list(chain.from_iterable([find_special_characters_indexes(line) for line in engine_lines]))
+
+    return any(number_indexes[0] - 1 <= i <= number_indexes[1] for i in all_line_indexes)
+
+
+if __name__ == '__main__':
+    engine_schematics: list[str] = read_input('day3_1')
+    total_sum_of_engine_parts: int = 0
+
+    for line_index, line in enumerate(engine_schematics):
+        star_indexes = find_star_indexes(line)
+        if not star_indexes:
+            continue
+
+        surrounding_engine_lines = engine_schematics[max(line_index - 1, 0):min(line_index + 2, len(engine_schematics))]
+        total_sum_of_engine_parts += find_star_gear_ratios(star_indexes, surrounding_engine_lines)
+
+    print(total_sum_of_engine_parts)
